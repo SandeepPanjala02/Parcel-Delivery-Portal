66d1dc12cd0d0580327ade61f3867e544e5a5f43
SandeepPanjala02 <panjalasandeepgoud02@gmail.com>
2024-12-16 12:32:21 +0530

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ParcelListComponent } from './components/parcel-list/parcel-list.component';
+import { ParcelCreateComponent } from './components/parcel-create/parcel-create.component';
+import { UserListComponent } from './components/user-list/user-list.component';
+import { UserCreateComponent } from './components/user-create/user-create.component';
+import { UserEditComponent } from './components/user-edit/user-edit.component';
+import { ParcelEditComponent } from './components/parcel-edit/parcel-edit.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: 'parcel-list',component:ParcelListComponent},
+  { path: 'parcel-create',component:ParcelCreateComponent},
+  { path: 'parcel-edit/:parcelId',component:ParcelEditComponent},
+  { path: 'user-list',component:UserListComponent},
+  { path: 'user-create',component:UserCreateComponent},
+  { path: 'user-edit/:userId', component:UserEditComponent}
+
+  
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,39 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule, provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ParcelListComponent } from './components/parcel-list/parcel-list.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ParcelCreateComponent } from './components/parcel-create/parcel-create.component';
+import { provideHttpClient } from '@angular/common/http';
+import { UserListComponent } from './components/user-list/user-list.component';
+import { UserCreateComponent } from './components/user-create/user-create.component';
+import { UserEditComponent } from './components/user-edit/user-edit.component';
+import { ParcelEditComponent } from './components/parcel-edit/parcel-edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ParcelListComponent,
+    ParcelCreateComponent,
+    UserListComponent,
+    UserCreateComponent,
+    UserEditComponent,
+    ParcelEditComponent,
+
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+
+
   ],
   providers: [
-    provideClientHydration(withEventReplay())
+    provideHttpClient()
   ],
   bootstrap: [AppComponent]
 })
